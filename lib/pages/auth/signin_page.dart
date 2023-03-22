e7fa1b92a7936763383723428a2a8f0a263804a4
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-03-22 16:02:46 +0530

    Added Sign In and Sign Up on the same screen

--- lib/pages/auth/signin_page.dart
+++ lib/pages/auth/signin_page.dart
@@ -3,6 +3,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace
 
 // Flutter Dependencies
+import 'package:flip_card/flip_card.dart';
+import 'package:flip_card/flip_card_controller.dart';
 import 'package:flutter/material.dart';
 // Imported Dependencies
 import 'package:provider/provider.dart';
@@ -11,6 +13,7 @@ import 'package:quiz_flutter/Models/theme.dart';
 import 'package:quiz_flutter/apis/firebase.dart';
 import 'package:quiz_flutter/pages/auth/signup_page.dart';
 import 'package:quiz_flutter/styles/appbar.dart';
+import 'package:quiz_flutter/styles/buttons.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -23,36 +26,73 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     FirebaseHelper firebaseHelper = FirebaseHelper();
+    FlipCardController cardKey = FlipCardController();
 
     return Consumer(
       builder: (context, ThemeModel themeNotifier, child) {
         return Scaffold(
           appBar: standardAppBar(themeNotifier, 'My Tech Quiz'),
           body: Center(
-            child: Card(
-              elevation: 20,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: MediaQuery.of(context).size.height * 0.5,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 30),
-                      child: Text(
-                        'Sign In',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headlineLarge,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox.shrink(),
+                  FlipCard(
+                    fill: Fill.fillBack,
+                    controller: cardKey,
+                    flipOnTouch: false,
+                    direction: FlipDirection.HORIZONTAL,
+                    front: Card(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(top: 30),
+                              child: Text(
+                                'Sign In',
+                                textAlign: TextAlign.center,
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              ),
+                            ),
+                            otherOption(cardKey),
+                          ],
+                        ),
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () => {firebaseHelper.googleSignIn(context)},
-                      child: buttonDesign('google', "Google Sign In"),
+                    back: Card(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        height: MediaQuery.of(context).size.height * 0.5,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(top: 30),
+                              child: Text(
+                                'Sign Up',
+                                textAlign: TextAlign.center,
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              ),
+                            ),
+                            otherOption(cardKey),
+                          ],
+                        ),
+                      ),
                     ),
-                    otherOption(),
-                  ],
-                ),
+                  ),
+                  SignInProviderButton(() {
+                    firebaseHelper.googleSignIn(context);
+                  }, 'google', "Google", context),
+                ],
               ),
             ),
           ),
@@ -61,45 +101,14 @@ class _SignInPageState extends State<SignInPage> {
     );
   }
 
-  buttonDesign(String image, String title) => Container(
-        height: 50,
-        width: 200,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              height: 30.0,
-              width: 30.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/$image.png'),
-                  fit: BoxFit.cover,
-                ),
-                shape: BoxShape.circle,
-              ),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Text(
-              title,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          ],
-        ),
-      );
-
-  otherOption() => Row(
+  otherOption(FlipCardController cardKey) => Row(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text("Don't have an account?"),
           TextButton(
             onPressed: () {
-              Navigator.popUntil(context, (route) => route.isFirst);
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const SignUpPage()));
+              cardKey.toggleCard();
             },
             child: Text(
               'Sign Up',
