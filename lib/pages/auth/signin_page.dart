a64427b9889b18880327db2b313ee30598ca0b6c
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-03-22 15:14:56 +0530

    Sign Up Page

--- lib/pages/auth/signin_page.dart
+++ lib/pages/auth/signin_page.dart
@@ -1,6 +1,6 @@
 // This file contains the code for the sign in page
 
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace
 
 // Flutter Dependencies
 import 'package:flutter/material.dart';
@@ -9,6 +9,7 @@ import 'package:provider/provider.dart';
 // Project Dependencies
 import 'package:quiz_flutter/Models/theme.dart';
 import 'package:quiz_flutter/apis/firebase.dart';
+import 'package:quiz_flutter/pages/auth/signup_page.dart';
 import 'package:quiz_flutter/styles/appbar.dart';
 
 class SignInPage extends StatefulWidget {
@@ -28,19 +29,31 @@ class _SignInPageState extends State<SignInPage> {
         return Scaffold(
           appBar: standardAppBar(themeNotifier, 'My Tech Quiz'),
           body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Sign In',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headlineLarge,
+            child: Card(
+              elevation: 20,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: Text(
+                        'Sign In',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => {firebaseHelper.googleSignIn(context)},
+                      child: buttonDesign('google', "Google Sign In"),
+                    ),
+                    otherOption(),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () => {firebaseHelper.googleSignIn(context)},
-                  child: buttonDesign('google', "Google Sign In"),
-                ),
-              ],
+              ),
             ),
           ),
         );
@@ -49,6 +62,7 @@ class _SignInPageState extends State<SignInPage> {
   }
 
   buttonDesign(String image, String title) => Container(
+        height: 50,
         width: 200,
         child: Row(
           mainAxisSize: MainAxisSize.min,
@@ -76,5 +90,22 @@ class _SignInPageState extends State<SignInPage> {
         ),
       );
 
-  signup(BuildContext context) {}
+  otherOption() => Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Don't have an account?"),
+          TextButton(
+            onPressed: () {
+              Navigator.popUntil(context, (route) => route.isFirst);
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const SignUpPage()));
+            },
+            child: Text(
+              'Sign Up',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ),
+        ],
+      );
 }
