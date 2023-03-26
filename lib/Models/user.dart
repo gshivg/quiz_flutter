9bba3ecf1cb82e74d02b53287ad223f7f3c16166
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-03-26 22:00:45 +0530

    Finalised User Model

--- lib/Models/user.dart
+++ lib/Models/user.dart
@@ -1,7 +1,33 @@
-
 class UserModel {
   late String uid;
   late String name;
+  late String email;
   late String age;
-  UserModel(this.uid);
+  late int? imgId;
+
+  UserModel({
+    required this.email,
+    required this.name,
+    required this.uid,
+    required this.age,
+    this.imgId,
+  });
+
+  UserModel.fromMap(Map<String, dynamic> map) {
+    uid = map['uid'];
+    name = map['fullName'];
+    email = map['email'];
+    age = map['age'];
+    imgId = map['imgId'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "uid": uid,
+      "name": name,
+      "email": email,
+      "age": age,
+      "imgId": imgId,
+    };
+  }
 }
