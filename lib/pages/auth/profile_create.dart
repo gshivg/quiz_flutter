223b2dc258581aa8949c6bdae09d547e1170ec63
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-04-22 23:39:19 +0530

    Basic Create Profile Page

--- lib/pages/auth/profile_create.dart
+++ lib/pages/auth/profile_create.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quiz_flutter/Models/theme.dart';
 import 'package:quiz_flutter/Models/user.dart';
+import 'package:quiz_flutter/styles/appbar.dart';
 
 class CreateProfilePage extends StatefulWidget {
-  const CreateProfilePage({super.key, required this.userModel, required this.firebaseUser});
+  const CreateProfilePage(
+      {super.key, required this.userModel, required this.firebaseUser});
   final UserModel userModel;
   final User firebaseUser;
   @override
@@ -13,6 +19,15 @@ class CreateProfilePage extends StatefulWidget {
 class _CreateProfilePageState extends State<CreateProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer(
+      builder: (context, ThemeModel themeNotifier, child) {
+        return Scaffold(
+          appBar: standardAppBar(themeNotifier, 'My Tech Quiz', context, false),
+          body: Center(
+            child: Text('Create Profile Page'),
+          ),
+        );
+      },
+    );
   }
 }
