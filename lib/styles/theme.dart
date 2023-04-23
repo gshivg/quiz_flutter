daba87baf8dfb6472e8475aadf5c35a86c0e6f6c
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-04-23 12:19:37 +0530

    Multiple Theme Changes

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -1,5 +1,7 @@
 // This file contains the theme data for light and dark modes.
 
+// ignore_for_file: prefer_const_constructors
+
 // Flutter Dependencies
 import 'package:flutter/material.dart';
 
@@ -7,10 +9,42 @@ ThemeData darkTheme() => ThemeData(
       brightness: Brightness.dark,
       useMaterial3: true,
       fontFamily: 'Sono',
+      dialogTheme: DialogTheme(
+        backgroundColor: Colors.white54,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      drawerTheme: const DrawerThemeData(
+        elevation: 0,
+        shadowColor: Colors.transparent,
+      ),
+      listTileTheme: ListTileThemeData(
+        shape: RoundedRectangleBorder(),
+        textColor: Colors.white,
+        style: ListTileStyle.drawer,
+      ),
     );
 
 ThemeData lightTheme() => ThemeData(
       brightness: Brightness.light,
       useMaterial3: true,
       fontFamily: 'Sono',
+      dialogTheme: DialogTheme(
+        backgroundColor: Colors.black45,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      drawerTheme: const DrawerThemeData(
+        elevation: 0,
+        shadowColor: Colors.transparent,
+      ),
+      listTileTheme: ListTileThemeData(
+        shape: RoundedRectangleBorder(),
+        textColor: Colors.black,
+        style: ListTileStyle.drawer,
+      ),
     );
