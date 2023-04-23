2ffaf07b81db39b56886f4fe605eb2d15953133a
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-04-23 18:24:55 +0530

    Home Page UI Completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,15 +1,16 @@
 // This file contains the MyHomePage class which is the home page of the app.
 
-// ignore_for_file: must_be_immutable, prefer_const_constructors, use_build_context_synchronously
+// ignore_for_file: must_be_immutable, prefer_const_constructors, use_build_context_synchronously, prefer_const_literals_to_create_immutables
 
 // Flutter Dependencies
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 // Imported Dependencies
 import 'package:provider/provider.dart';
 // Project Dependencies
 import 'package:quiz_flutter/Models/theme.dart';
-import 'package:quiz_flutter/apis/firebase/user_firebase.dart';
 import 'package:quiz_flutter/pages/auth/auth_page.dart';
 import 'package:quiz_flutter/shared_preferences/signin.dart';
 import 'package:quiz_flutter/styles/appbar.dart';
@@ -38,22 +39,27 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 ListTile(
                   title: Text('My Profile'),
+                  leading: Icon(Icons.person_outlined),
                   onTap: () => Fluttertoast.showToast(msg: 'Profile'),
                 ),
                 ListTile(
-                  title: Text('My High Scores'),
-                  onTap: () => Fluttertoast.showToast(msg: 'High Scores'),
-                ),
-                ListTile(
                   title: Text('Show Friends'),
+                  leading: Icon(Icons.people_outlined),
                   onTap: () => Fluttertoast.showToast(msg: 'Show Friends'),
                 ),
                 ListTile(
+                  title: Text('Leaderboard'),
+                  leading: Icon(Icons.leaderboard_outlined),
+                  onTap: () => Fluttertoast.showToast(msg: 'Leaderboard'),
+                ),
+                ListTile(
                   title: Text('Settings'),
+                  leading: Icon(Icons.settings_outlined),
                   onTap: () => Fluttertoast.showToast(msg: 'Settings'),
                 ),
                 ListTile(
                   title: Text('Sign Out'),
+                  leading: Icon(Icons.logout_outlined),
                   onTap: () => {
                     Navigator.pop(context),
                     UIHelper.showConfirmationDialog(
@@ -63,30 +69,105 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                'Home Page',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              UIHelper.showConfirmationDialog(
-                  'Confirm Exit?', context, signOut);
-            },
-            tooltip: 'Increment',
-            child: const Icon(
-              Icons.logout,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                homeScreenTileRow(
+                  'Standard Mode',
+                  Icons.star_border_outlined,
+                  onPressed: comingSoon,
+                ),
+                homeScreenTileRow(
+                  'Rapid Fire',
+                  Icons.timer_outlined,
+                  onPressed: comingSoon,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      homeScreenTileColumn(
+                        'Play with Friends',
+                        Icons.people_outline,
+                        onPressed: comingSoon,
+                      ),
+                      homeScreenTileColumn(
+                        'Play with Randoms',
+                        Icons.signpost_outlined,
+                        onPressed: comingSoon,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         );
       },
     );
+  }
+
+  homeScreenTileRow(String title, IconData icon,
+          {double elevation = 5, VoidCallback? onPressed}) =>
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: ElevatedButton(
+            onPressed: onPressed,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    icon,
+                    size: 40,
+                  ),
+                  SizedBox(width: 15),
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+  homeScreenTileColumn(String title, IconData icon,
+          {double elevation = 5, VoidCallback? onPressed}) =>
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: ElevatedButton(
+            onPressed: onPressed,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    icon,
+                    size: 35,
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+  void comingSoon() {
+    log('Feature Coming Soon');
+    Fluttertoast.showToast(msg: 'Feature Coming Soon');
   }
 
   void signOut() async {
@@ -98,11 +179,5 @@ class _MyHomePageState extends State<MyHomePage> {
         builder: (context) => const AuthPage(),
       ),
     );
-  }
-
-  void checkProfile() async {
-    SignInPreferences signInPreferences = SignInPreferences();
-    uid = (await signInPreferences.getSignIn())!;
-    UserHelper.checkProfileCreated(uid, context);
   }
 }
