1a00a7ee980c21337c0a5deb685ba563a0e2087d
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-04-23 11:43:45 +0530

    Added Drawer and check profile

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,34 +1,75 @@
 // This file contains the MyHomePage class which is the home page of the app.
 
-// ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable, prefer_const_constructors
 
 // Flutter Dependencies
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 // Imported Dependencies
 import 'package:provider/provider.dart';
 // Project Dependencies
 import 'package:quiz_flutter/Models/theme.dart';
+import 'package:quiz_flutter/apis/firebase/user_firebase.dart';
 import 'package:quiz_flutter/pages/auth/auth_page.dart';
 import 'package:quiz_flutter/pages/quizpage.dart';
+import 'package:quiz_flutter/shared_preferences/signin.dart';
 import 'package:quiz_flutter/styles/appbar.dart';
+import 'package:quiz_flutter/utils/ui_helper.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key, required this.signInKey});
-  String signInKey;
+  const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String uid = '';
   @override
   Widget build(BuildContext context) {
+    checkProfile();
+
     return Consumer(
       builder: (context, ThemeModel themeNotifier, child) {
         return Scaffold(
-          appBar: standardAppBar(themeNotifier, 'Home Page', context, false),
-          body: Center(
+          appBar: drawerAppBar(themeNotifier, 'Home Page', context),
+          drawer: Drawer(
+            elevation: 5,
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  child: Text('Drawer Header'),
+                ),
+                ListTile(
+                  title: Text('My Profile'),
+                  onTap: () => Fluttertoast.showToast(msg: 'Profile'),
+                ),
+                ListTile(
+                  title: Text('My High Scores'),
+                  onTap: () => Fluttertoast.showToast(msg: 'High Scores'),
+                ),
+                ListTile(
+                  title: Text('Show Friends'),
+                  onTap: () => Fluttertoast.showToast(msg: 'Show Friends'),
+                ),
+                ListTile(
+                  title: Text('Settings'),
+                  onTap: () => Fluttertoast.showToast(msg: 'Settings'),
+                ),
+                ListTile(
+                  title: Text('Sign Out'),
+                  onTap: () => {
+                    Navigator.pop(context),
+                    UIHelper.showConfirmationDialog(
+                        'Sign Out?', context, signOut)
+                  },
+                ),
+              ],
+            ),
+          ),
+          body: Container(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Text(
                   'Home Page',
@@ -40,13 +81,8 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
-              Navigator.popUntil(context, (route) => route.isFirst);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AuthPage(),
-                ),
-              );
+              UIHelper.showConfirmationDialog(
+                  'Confirm Exit?', context, signOut);
             },
             tooltip: 'Increment',
             child: const Icon(
@@ -56,5 +92,22 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       },
     );
+  }
+
+  void signOut() async {
+    SignInPreferences signInPreferences = SignInPreferences();
+    signInPreferences.deleteSignIn();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const AuthPage(),
+      ),
+    );
+  }
+
+  void checkProfile() async {
+    SignInPreferences signInPreferences = SignInPreferences();
+    uid = (await signInPreferences.getSignIn())!;
+    UserHelper.checkProfileCreated(uid, context);
   }
 }
