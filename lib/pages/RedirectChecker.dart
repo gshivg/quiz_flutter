6f1bffdaef3fb6f8057cdd1068c70e7499b7a7cf
Shivansh Gupta <95837362+gshivg@users.noreply.github.com>
2023-04-29 12:49:52 +0530

    Skeleton

--- lib/pages/RedirectChecker.dart
+++ lib/pages/RedirectChecker.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: must_be_immutable, use_build_context_synchronously, file_names
+// ignore_for_file: must_be_immutable, use_build_context_synchronously, file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:quiz_flutter/apis/firebase/user_firebase.dart';
 import 'package:quiz_flutter/shared_preferences/signin.dart';
+import 'package:skeletons/skeletons.dart';
 
 class RedirectChecker extends StatefulWidget {
   const RedirectChecker({super.key});
@@ -16,9 +17,12 @@ class _RedirectCheckerState extends State<RedirectChecker> {
   @override
   Widget build(BuildContext context) {
     checkProfile();
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: SkeletonListView(
+          spacing: 15,
+        ),
       ),
     );
   }
